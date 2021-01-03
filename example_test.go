fabbf6187d0df7b0000b58d8d3347e29383e3eaa
Janis Horsts <janis.horsts@gmail.com>
2021-01-03 15:30:14 +0000

    Initial (#1)
    * initial commit
    
    Co-authored-by: Jānis Horsts <jhorsts@gmail.com>

---
+++ example_test.go
@@ -0,0 +1,54 @@
+package request_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+
+	"go.expect.digital/request"
+)
+
+func ExampleDecode() {
+	r := httptest.NewRequest(
+		http.MethodPost,
+		"/?filterType=pending,approved&clientId=4&filterClientids=1|2|3",
+		strings.NewReader(`{"id":1}`),
+	)
+
+	var req struct {
+		// query params
+		FilterType      []string `query:"filterType,imploded"`
+		ClientId        int
+		FilterClientIds []int `query:",pipe,imploded"`
+
+		// body
+		Client struct {
+			Id int
+		} `body:"json"`
+	}
+
+	_ = request.Decode(r, &req)
+
+	fmt.Printf("%+v\n", req)
+	// Output:
+	// {FilterType:[pending approved] ClientId:4 FilterClientIds:[1 2 3] Client:{Id:1}}
+}
+
+func ExampleDecoder() {
+	r := httptest.NewRequest(http.MethodPost, "/?ids=1,2,3", nil)
+
+	var req struct {
+		Ids []int
+	}
+
+	decoder := request.NewDecoder()
+	// set query values imploded "?ids=1,2,3" by default
+	decoder.Query.Exploded = false
+
+	_ = decoder.Decode(r, &req)
+
+	fmt.Printf("%+v\n", req)
+	// Output:
+	// {Ids:[1 2 3]}
+}
